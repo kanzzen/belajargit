1f0bd922d94e0f5337c9b02e9e10c729ae896398
kanzzen <willyfebrian27@gmail.com>
2023-05-26 10:41:43 +0700

    Initial Commit

---
+++ belajargit/belajargitApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajargitApp.swift
+//  belajargit
+//
+//  Created by Wilson Tan on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct belajargitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
